d7e109c5b70c97748831bef8872da2240ecebbf3
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-06-01 09:37:20 +1000

    Add syncoid configuration for backup user and packages

---
+++ modules/profiles/server/backup/syncoid.nix
@@ -0,0 +1,16 @@
+{
+  flake.modules.nixos.syncoid = {
+    users.users.backup = {
+      isNormalUser = true;
+      createHome = false;
+      home = "/var/empty";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGS8uEXOYevF6nZK+2zfUTm4k1Gh1RDYP3nyTG4raPBL syncoid"
+      ];
+    };
+    environment.systemPackages = [
+      pkgs.lzop
+      pkgs.mbuffer
+    ];
+  };
+}
