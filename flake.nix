6205571656097727f596cc1c7017f521a23d756a
sam <samblackabc@gmail.com>
2026-06-01 21:13:15 +1000

    skylite

--- flake.nix
+++ flake.nix
@@ -27,6 +27,7 @@
     nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
     nixpkgs-lib.follows = "nixpkgs";
     preservation.url = "github:nix-community/preservation";
+    quadlet-nix.url = "github:SEIAROTg/quadlet-nix";
     secrets = {
       url = "git+ssh://git@github.com/SamIAm789/secrets";
       flake = false;
