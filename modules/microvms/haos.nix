b1056edc637ecdec51d060dfe8b90ddb63d9c8a0
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-06-01 12:23:01 +1000

    Update haos.nix

--- modules/microvms/haos.nix
+++ modules/microvms/haos.nix
@@ -56,8 +56,7 @@
   --disk path=/persist/microvms/haos/haos_ova-17.3.qcow2 \
   --net tap=vm-haos,mac=${mac} \
   --serial tty \
-  --api-socket /run/haos-vm/ch.sock \
-  --kernel /dev/null
+  --api-socket /run/haos-vm/ch.sock
 '';
 
         ExecStop = ''
