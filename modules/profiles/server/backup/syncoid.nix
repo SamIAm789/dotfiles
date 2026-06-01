ff4e22eb80eb48a10fec4d435bb79376643bde22
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-06-01 10:35:58 +1000

    Update syncoid.nix

--- modules/profiles/server/backup/syncoid.nix
+++ modules/profiles/server/backup/syncoid.nix
@@ -1,16 +1,21 @@
 {
-  flake.modules.nixos.syncoid = {
-    users.users.backup = {
-      isNormalUser = true;
-      createHome = false;
-      home = "/var/empty";
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGS8uEXOYevF6nZK+2zfUTm4k1Gh1RDYP3nyTG4raPBL syncoid"
+  flake.modules.nixos.syncoid =
+    {
+      pkgs,
+      ...
+    }:
+    {
+      users.users.backup = {
+        isNormalUser = true;
+        createHome = false;
+        home = "/var/empty";
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGS8uEXOYevF6nZK+2zfUTm4k1Gh1RDYP3nyTG4raPBL syncoid"
+        ];
+      };
+      environment.systemPackages = [
+        pkgs.lzop
+        pkgs.mbuffer
       ];
     };
-    environment.systemPackages = [
-      pkgs.lzop
-      pkgs.mbuffer
-    ];
-  };
 }
