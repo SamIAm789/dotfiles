7172cbf00afdd6d62072c43d774805815f5bdba5
sam <samblackabc@gmail.com>
2026-06-02 16:12:47 +1000

    deploy key framework

--- modules/hosts/framework/deploy-key.nix
+++ modules/hosts/framework/deploy-key.nix
@@ -13,7 +13,7 @@
     systemd.tmpfiles.rules = [
       "d /etc/ssh/keys 0700 root root -"
     ];
-    sops.secrets."framwork-dotfiles-deploy" = {
+    sops.secrets."framework-dotfiles-deploy" = {
       sopsFile = "${inputs.secrets}/secrets/secrets.yaml";
       owner = "root";
       group = "root";
