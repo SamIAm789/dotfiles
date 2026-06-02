de67d07ca116e7f061b24c2efad111d405df086a
sam <samblackabc@gmail.com>
2026-06-02 14:07:41 +1000

    deploy key framework

---
+++ modules/hosts/framework/deploy-key.nix
@@ -0,0 +1,37 @@
+{
+  flake.modules.nixos.deploy-key-framework =
+  let
+
+    keyPath = "/etc/ssh/keys/secrets-deploy";
+  in
+  {
+
+      systemd.tmpfiles.rules = [
+        "d /etc/ssh/keys 0700 root root -"
+      ];
+
+      sops.secrets."deploy-key" = {
+        sopsFile = "${inputs.secrets}/secrets/secrets.yaml";
+        owner = "root";
+        group = "root";
+        mode = "0600";
+        path = keyPath;
+      };
+
+      systemd.tmpfiles.rules += [
+        "d /root/.ssh 0700 root root -"
+      ];
+
+      environment.etc."ssh/ssh_config.d/deploy-key.conf".text = ''
+        Host github.com
+          IdentityFile ${keyPath}
+          IdentitiesOnly yes
+      '';
+
+      programs.ssh.knownHosts.github = {
+        hostNames = [ "github.com" ];
+        publicKey = "github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMjv8L5XpTuJJhbpZisF/zLDA0zPMSvHdkr4UvCOqU";
+      };
+    };
+  };
+}
