7c9f5f53fa0cb3657d981972a9ebda08481488c4
sam <samblackabc@gmail.com>
2026-06-05 23:09:13 +1000

    quadlet

--- modules/profiles/base/fish.nix
+++ modules/profiles/base/fish.nix
@@ -13,10 +13,12 @@
 
       programs.bash = {
         interactiveShellInit = ''
-          if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-          then
-            shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-            exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+          if [[ $USER = "sam" ]]; then
+            if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+            then
+              shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+              exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+            fi
           fi
         '';
       };
