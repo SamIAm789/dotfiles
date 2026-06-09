e24692cfd061c16dac443a6f5f2ee6e49d28c9f5
sam <samblackabc@gmail.com>
2026-06-09 15:16:29 +1000

    github access

--- modules/profiles/server/github-keys.nix
+++ modules/profiles/server/github-keys.nix
@@ -28,8 +28,6 @@
       };
 
       sops = {
-        defaultSopsFile = "${inputs.secrets}/secrets.yaml";
-        age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
         secrets.github-bot-key = {
           owner = deployUser;
