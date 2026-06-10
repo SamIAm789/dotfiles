eb647fabaabbaebcfb9a7647f888834971394227
sam <samblackabc@gmail.com>
2026-06-10 15:43:48 +1000

    esphome module

--- modules/hosts/framework/esphome.nix
+++ modules/hosts/framework/esphome.nix
@@ -7,6 +7,15 @@
         "char-ttyS"
       ];
     };
+    programs.firefox = {
+      policies = {
+        DefaultSerialGuardSetting = 3; # Allows the API usage
+        Preferences = {
+          "dom.webserial.enabled" = true;
+        };
+      };
+    };
+
     users.users.sam = {
       extraGroups = [ "dialout" ];
     };
