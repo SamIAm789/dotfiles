9a05905d4d1cbfc7a527cf38407d255950f94808
sam <samblackabc@gmail.com>
2026-06-17 22:31:05 +1000

    opencloud

--- modules/services/opencloud.nix
+++ modules/services/opencloud.nix
@@ -5,7 +5,7 @@
     services.opencloud = {
       enable = true;
       address = "0.0.0.0";
-      url = "100.100.0.4:9200";
+      url = "http://100.100.0.4:9200";
       port = 9200;
       stateDir = "/stuff/opencloud";
       environment = {
