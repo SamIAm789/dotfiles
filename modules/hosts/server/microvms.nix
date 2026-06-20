0b594dd8743a3d56def6fda564e32f11b3f04ed2
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-06-20 18:01:58 +1000

    Update microvms.nix

--- modules/hosts/server/microvms.nix
+++ modules/hosts/server/microvms.nix
@@ -8,7 +8,7 @@
 
     imports = [
       (self.factory.nebulaSecrets { vm = "immich"; })
-      inputs.self.modules.nixos.haos-ch-working
+      inputs.self.modules.nixos.haos-ch
 inputs.self.modules.nixos.haos-share
     ];
 
