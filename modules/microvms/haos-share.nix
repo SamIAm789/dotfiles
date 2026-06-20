dfee03e3e40da79c2bf0f93ed6c1c95e1789e5e6
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-06-20 16:44:57 +1000

    Create haos-share.nix

---
+++ modules/microvms/haos-share.nix
@@ -0,0 +1,39 @@
+{
+  flake.modules.nixos.haos-share =
+  {
+    pkgs,
+    ...
+  }:
+  let
+    vmName = "haos";
+    dataShareDir = "/persist/microvms/haos/data";
+    virtiofsSocket = "/run/virtiofs-${vmName}-data.sock";
+    fsTag = "ha-data";
+  in
+  {
+    environment.systemPackages = [ pkgs.virtiofsd ];
+
+    systemd.services."virtiofsd-${vmName}-data" = {
+      description = "virtiofsd for ${vmName} data share";
+      wantedBy = [ "multi-user.target" ];
+      before = [ "${vmName}-vm.service" ];
+
+      serviceConfig = {
+        Type = "notify";
+        ExecStart = "\( {pkgs.virtiofsd}/bin/virtiofsd --socket-path= \){virtiofsSocket} --shared-dir=${dataShareDir} --cache=never --thread-pool-size=4";
+
+        RuntimeDirectory = "virtiofsd-${vmName}";
+        RuntimeDirectoryPreserve = "yes";
+
+        ExecStop = "${pkgs.coreutils}/bin/rm -f ${virtiofsSocket}";
+        Restart = "on-failure";
+
+        User = "root";
+        AmbientCapabilities = [ "CAP_SYS_ADMIN" ];
+        CapabilityBoundingSet = [ "CAP_SYS_ADMIN" ];
+        ReadWritePaths = [ dataShareDir ];
+        ProtectSystem = "strict";
+      };
+    };
+  };
+}
