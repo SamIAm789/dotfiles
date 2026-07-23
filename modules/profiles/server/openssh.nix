5b893abfda50dbff1fabbde3a78599bb8daf5307
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-07-23 10:00:45 +1000

    Update openssh.nix

--- modules/profiles/server/openssh.nix
+++ modules/profiles/server/openssh.nix
@@ -1,5 +1,9 @@
 {
   flake.modules.nixos.openssh = {
     services.openssh.enable = true;
+    listenAddresses = [
+      { addr = "0.0.0.0"; port = 22; }
+      { addr = "::"; port = 22; }
+    ];
   };
 }
