29d4c2a7c12f44946ab4e9ffb9380af495add7ac
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-07-23 22:49:57 +1000

    Update openssh.nix

--- modules/profiles/server/openssh.nix
+++ modules/profiles/server/openssh.nix
@@ -1,10 +1,14 @@
 {
   flake.modules.nixos.openssh = {
-    services.openssh.enable = true;
-    listenAddresses = [
-      { addr = "0.0.0.0"; port = 22; }
-      { addr = "::"; port = 22; }
-    ];
-    permitRootLogin = "no";
+    services.openssh = {
+      enable = true;
+      listenAddresses = [
+        { addr = "0.0.0.0"; port = 22; }
+        { addr = "::"; port = 22; }
+      ];
+      permitRootLogin = "no";
+    }
+  
+  networking.firewall.interfaces."nebula.pertaka".allowedTCPPorts = [ 22 ];
   };
 }
