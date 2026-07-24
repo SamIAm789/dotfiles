36c8faddfb5a128a07582c99398acc45c7d7abf8
sam <samblackabc@gmail.com>
2026-07-24 18:10:16 +1000

    thinkpad

--- modules/hosts/thinkpad/gnome.nix
+++ modules/hosts/thinkpad/gnome.nix
@@ -2,12 +2,15 @@
   flake.modules.nixos.thinkpad =
   {
     pkgs,
+    lib,
     ...
   }:
   {
 
     services.displayManager.gdm.enable = true;
     services.desktopManager.gnome.enable = true;
+
+    services.gnome.gcr-ssh-agent.enable = lib.mkForce false;
 
     environment.gnome.excludePackages = with pkgs; [
       gnome-music
