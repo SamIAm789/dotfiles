326cc82fa196f69dbf9430ecc97d8ef0ac83d478
sam <samblackabc@gmail.com>
2026-07-24 18:08:36 +1000

    thinkpad

--- modules/hosts/thinkpad/configuration.nix
+++ modules/hosts/thinkpad/configuration.nix
@@ -5,7 +5,7 @@
 {
   flake.nixosConfigurations = inputs.self.lib.mkNixos "x86_64-linux" "thinkpad";
 
-  flake.modules.nixos.thnkpad = {
+  flake.modules.nixos.thinkpad = {
 
     imports = with inputs.self.modules.nixos; [
       sam
