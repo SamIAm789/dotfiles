f850bf4fdfa32672220b3c0dc5b8b53b90fc1508
sam <samblackabc@gmail.com>
2026-07-31 21:49:38 +1000

    syncoid create backup user

--- modules/profiles/server/backup/syncoid.nix
+++ modules/profiles/server/backup/syncoid.nix
@@ -10,7 +10,7 @@
         createHome = false;
         home = "/var/empty";
         openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGS8uEXOYevF6nZK+2zfUTm4k1Gh1RDYP3nyTG4raPBL syncoid"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINHl0zA061dpCTG2lDEFiAd0IfXZ+GF/Cun9l7/WcD6B syncoid"
         ];
       };
       environment.systemPackages = [
