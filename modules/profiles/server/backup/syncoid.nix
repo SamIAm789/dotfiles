f58d6fd301e90be6fac69a3ac6906f0acc215c4f
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-08-01 11:49:00 +1000

    Update syncoid.nix

--- modules/profiles/server/backup/syncoid.nix
+++ modules/profiles/server/backup/syncoid.nix
@@ -6,16 +6,28 @@
     }:
     {
       users.users.backup = {
-        isNormalUser = true;
-        createHome = false;
-        home = "/var/empty";
+        isSystemUser = true;
+        group = "backup";
+        home = "/var/lib/backup";
+        createHome = true;
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINHl0zA061dpCTG2lDEFiAd0IfXZ+GF/Cun9l7/WcD6B syncoid"
         ];
       };
+
+      users.groups.backup = {};
+
       environment.systemPackages = [
         pkgs.lzop
         pkgs.mbuffer
       ];
+
+      services.openssh.extraConfig = ''
+        Match User backup
+        PasswordAuthentication no
+        X11Forwarding no
+        AllowTcpForwarding no
+        PermitTTY no
+      '';
     };
 }
