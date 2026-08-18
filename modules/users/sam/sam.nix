ae661471914bace44d6920dd46aec62991cc921c
sam <samblackabc@gmail.com>
2026-08-18 16:59:02 +1000

    enabled auto subuid for sam

--- modules/users/sam/sam.nix
+++ modules/users/sam/sam.nix
@@ -35,6 +35,7 @@ in
           "wheel"
           "networkmanager"
         ];
+        autoSubIdRange = true;
         hashedPasswordFile = config.sops.secrets.sam.path;
        # initialHashedPassword = lib.mkDefault "$y$j9T$isUS3neJEEFmJTYteyeHx1$RG2NFoIf.eBb0rELDl1aTCP0c4aC/33GpIKzFkCIKm2";
       };
