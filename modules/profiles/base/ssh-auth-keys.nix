6eefb34f87046f4552496a9fb4ad5db296bcca62
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-08-27 19:50:38 +1000

    Update ssh-auth-keys.nix

--- modules/profiles/base/ssh-auth-keys.nix
+++ modules/profiles/base/ssh-auth-keys.nix
@@ -4,7 +4,7 @@
 
       users.users.sam.openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINHptkyokSn8XreYaJQUyy1UPF8qiAq2cjCat3zPMO5Z user" #framework
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMHV+lxnSONakDVuDrD3qcKWLKj4JCu05VEe8EeyCDIB u0_a402@localhost" #termux
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICxyLkFyzrqtcxLIK+H814/cd+OsvGva3IpHK/Iuey1F connectbot" #connectbot
         "AAAAC3NzaC1lZDI1NTE5AAAAIOS0RjZECCnmebiu1lw0LY9KaTthlUesI8AeqvtXg5fR" #server
       ];
     };
