3e70c453b1a8b8606df854f966bb8dc2a2b4af0d
sam <samblackabc@gmail.com>
2026-08-28 22:24:50 +1000

    added backup personal ssh key

--- modules/profiles/base/ssh-auth-keys.nix
+++ modules/profiles/base/ssh-auth-keys.nix
@@ -6,6 +6,7 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINHptkyokSn8XreYaJQUyy1UPF8qiAq2cjCat3zPMO5Z user" #framework
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICxyLkFyzrqtcxLIK+H814/cd+OsvGva3IpHK/Iuey1F connectbot" #connectbot
         "AAAAC3NzaC1lZDI1NTE5AAAAIOS0RjZECCnmebiu1lw0LY9KaTthlUesI8AeqvtXg5fR" #server
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDERLAU69/5R61n5Oiv/VTc8RCKfuj/wbc3XJIJZXZ2D sam@backup" #backup
       ];
     };
 }
