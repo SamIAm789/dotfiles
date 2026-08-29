616bd3b7c1c57339f1217d454fc5e2b1ae22ca4d
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-08-29 12:01:28 +1000

    Create dnsmasq.nix

---
+++ modules/_wip/router/dnsmasq.nix
@@ -0,0 +1,36 @@
+{
+  flake.modules.nixos.router = {
+
+    services.dnsmasq = {
+    enable = true;
+    resolveLocalQueries = true;
+
+    settings = {
+      interface = [ lanIF "lo" ];
+      bind-interfaces = true;
+      except-interface = [ wanIF ];
+
+      # DHCP
+      dhcp-authoritative = true;
+      dhcp-range = [ "\( {lanNet}.100, \){lanNet}.200,12h" ];
+      dhcp-option = [
+        "option:router,${router}"
+        "option:dns-server,${router}"
+        "option:domain-search,${domain}"
+      ];
+
+      # Local DNS
+      domain = domain;
+      local = "/${domain}/";
+      expand-hosts = true;
+      domain-needed = true;
+      bogus-priv = true;
+      no-resolv = true;
+      no-hosts = true;
+
+      # Everything else → unbound
+      server = [ "127.0.0.1" ];
+
+      cache-size = 1000;
+    };
+  };
