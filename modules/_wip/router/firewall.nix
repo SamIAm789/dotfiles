db6c3f2ffb9c70cec18dfddbb0e9a2afa71fff73
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-08-30 08:35:16 +1000

    Update firewall.nix

--- modules/_wip/router/firewall.nix
+++ modules/_wip/router/firewall.nix
@@ -1,5 +1,31 @@
 {
-  flake.modules.nixos.router = {
+  flake.modules.nixos.router =
+  let
+  
+  # Private / reserved ranges that should never appear as source on WAN
+    bogons4 = [
+      "0.0.0.0/8"
+      "10.0.0.0/8"
+      "100.64.0.0/10"
+      "127.0.0.0/8"
+      "169.254.0.0/16"
+      "172.16.0.0/12"
+      "192.0.0.0/24"
+      "192.0.2.0/24"
+      "192.168.0.0/16"
+      "198.18.0.0/15"
+      "198.51.100.0/24"
+      "203.0.113.0/24"
+    ];
+
+    bogons6 = [
+      "::/128"
+      "::1/128"
+      "fc00::/7"
+      "fe80::/10"
+    ];
+  in
+  {
 
     networking.nftables.enable = true;
 
@@ -9,14 +35,8 @@
       filterForward = true;
       trustedInterfaces = [ ${lanIF} ];
       extraInputRules = ''
-        iifname "${cfg.wanIF}" ip saddr {
-    0.0.0.0/8, 10.0.0.0/8, 100.64.0.0/10, 127.0.0.0/8,
-    169.254.0.0/16, 172.16.0.0/12, 192.0.0.0/24, 192.0.2.0/24,
-    192.168.0.0/16, 198.18.0.0/15, 198.51.100.0/24, 203.0.113.0/24
-  } drop
-  iifname "${cfg.wanIF}" ip6 saddr {
-    ::/128, ::1/128, fc00::/7, fe80::/10
-  } drop
+        iifname "${wanIF}" ip saddr ${mkNftSet bogons4} drop
+        iifname "${wanIF}" ip6 saddr ${mkNftSet bogons6} drop
       '';
       extraForwardRules = ''
         iifname "\( ${lanIF}" oifname " \)${wanIF}" accept
