d7f9692d6379d6e9f96a3c501265beece91231d3
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-08-30 07:14:23 +1000

    Update constants-let-in.nix

--- modules/_wip/router/constants-let-in.nix
+++ modules/_wip/router/constants-let-in.nix
@@ -1,3 +1,14 @@
 {
+  flake.modules.nixos.router = 
   let
-  
+    wanIF     = "eth0";
+    lanIF     = "br-lan";
+    lanNet    = "10.25.0";
+    routerIP  = "10.25.0.1";
+    lanCIDR   = "10.25.0.0/24";
+    domain    = "internal";
+    dhcpStart = "10.25.0.100";
+    dhcpEnd   = "10.25.0.200";
+  in
+  { };
+}
