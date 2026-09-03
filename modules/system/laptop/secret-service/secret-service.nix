4e1a50ac7229ff007e2255f11dbf2fa4fa1140b7
sam <samblackabc@gmail.com>
2026-09-04 08:26:57 +1000

    gnome keyring remove gnome ssh agent

--- modules/system/laptop/secret-service/secret-service.nix
+++ modules/system/laptop/secret-service/secret-service.nix
@@ -1,8 +1,10 @@
 {
   flake.modules.nixos.secret-service = {
     security.pam.services.login.enableGnomeKeyring = true;
-    services.gnome.gnome-keyring.enable = true;
-    gcr-ssh-agent.enable = false;
+    services.gnome = {
+      gnome-keyring.enable = true;
+      gcr-ssh-agent.enable = false;
+    };
   };
 
   flake.modules.homeManager.secret-service= {
