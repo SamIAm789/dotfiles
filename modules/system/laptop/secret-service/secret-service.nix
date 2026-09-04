1ca4d3b936c8675c7a6893e89df0ec8fcc6956ad
sam <samblackabc@gmail.com>
2026-09-04 10:48:23 +1000

    added libsecret to secret service module

--- modules/system/laptop/secret-service/secret-service.nix
+++ modules/system/laptop/secret-service/secret-service.nix
@@ -1,9 +1,17 @@
 {
-  flake.modules.nixos.secret-service = {
+  flake.modules.nixos.secret-service =
+  {
+    pkgs,
+    ...
+  }:
+  {
     security.pam.services.login.enableGnomeKeyring = true;
     services.gnome = {
       gnome-keyring.enable = true;
       gcr-ssh-agent.enable = false;
     };
+    environment.systemPackages = with pkgs; [
+      libsecret
+    ];
   };
 }
