d71b736dc52309eb9c981838555c82c67a7063cd
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-09-05 18:50:26 +1000

    Create hermes/hermes-user.nix

---
+++ modules/_wip/hermes/hermes-user.nix
@@ -0,0 +1,12 @@
+users.groups.hermes = {};
+
+users.users.hermes = {
+  isSystemUser = true;
+  group = "hermes";
+  home = "/var/lib/hermes";
+  createHome = true;
+
+  openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAA... hermes@hermes"
+  ];
+};
