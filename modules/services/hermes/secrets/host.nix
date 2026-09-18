14ea791ea2f8cfc5b2b6e86ed01c820529ef26c1
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-09-19 08:37:38 +1000

    Update host.nix

--- modules/services/hermes/secrets/host.nix
+++ modules/services/hermes/secrets/host.nix
@@ -14,10 +14,16 @@
       format = "yaml";
     };
 
+    systemd.tmpfiles.rules = [
+      "d /run/hermes-secrets 0750 root microvm -"
+    ];
+
     sops.templates."hermes.env" = {
       path = "/run/hermes-secrets/hermes.env";
       content = config.sops.placeholder."hermes-env";
-      mode = "0400";
+      mode = "0440";
+      # owner = "root";
+      # group = "microvm";
     };
   };
 }
